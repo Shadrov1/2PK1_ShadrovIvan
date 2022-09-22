66a11b29089b64c5dcd1c9d68d1b802cd85f373f
Иван Шадров <dazooop33@gmail.com>
2022-09-22 09:47:50 +0500

    text edit

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("работа с git");
+            //int a = -50;
+            //int b = 0;
+            //for (; a <= b; a+=5)
+            //{
+              //  Console.WriteLine(a.ToString());
+            //}
+            //Console.WriteLine("Первое задание выполнено");
+            {
+                //char f = 'л';
+                //for (int i = 0; i <= 7; i++)
+                //{
+                    //Console.Write(f);
+                    //f++;
+                //}
+                //Console.WriteLine("Второе задание выполнено");
+            }
+           // char b = '#';
+           // for (var i = 0; i <= 8; i++)
+            {
+               //for (var j = 0; j < 4; j++)
+               // {
+                    //Console.Write('#');
+               // }
+               //Console.WriteLine();
+            }
+            //Console.WriteLine("Третье задание выполнено");
+            {
+               // for (int i = 0; i <= 100; i++)
+                    //if (i % 7 == 0) Console.WriteLine(i);
+            }
+            //Console.WriteLine("Третье задание выполнено");
+            {
+                //for (int i = 0, j = 45; i + j <= 17; i++, j--)
+                //{
+                  //  Console.WriteLine(i + j);
+                //}
+                
+            }
         }
     }
 }
