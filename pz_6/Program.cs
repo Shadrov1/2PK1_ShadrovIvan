dfbabebe600f0288878f41b2a89848b8a1af0414
Иван Шадров <dazooop33@gmail.com>
2022-09-26 19:07:15 +0500

    25_var

---
+++ pz_6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double t = 0;
+            double g = 9.8;
+            while (true)
+            {
+                t += 0.5;
+                double v = g * t;
+                Console.WriteLine(v);
+            }
+                
+        }
+    }
+}
