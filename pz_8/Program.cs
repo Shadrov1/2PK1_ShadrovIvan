c6697a691e7e3ea9ee7ca65effa07a0bb3cf0adc
Иван Шадров <dazooop33@gmail.com>
2022-10-05 23:31:56 +0500

    var_25

---
+++ pz_8/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace pz_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            double max = 10.0;
+            int i = 0, j = 0;
+            int n = 5;
+            int m = 5;
+            int[,] massiv = new int[n, m];
+            Random random = new Random();
+            double min = max / 2;
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                {
+                    massiv[i, j] = random.Next(-10, 10);
+                    Console.WriteLine(massiv[i, j]);
+                    if (massiv[i, j] > 0)
+                    {
+                        sum = sum + massiv[i, j];
+
+                    }
+                    if (massiv[i, j] < min)
+                    {
+                        min = massiv[i, j];
+                    }
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine(min);
+            Console.WriteLine(sum);
+            Console.WriteLine(min * sum);
+        }
+        }
+    }
