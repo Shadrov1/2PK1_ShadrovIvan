00662b7244f0b2d5e7a0bbf92bd848702f1a0cf5
Иван Шадров <dazooop33@gmail.com>
2022-10-26 13:27:33 +0500

    var_24

---
+++ pz_10/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace pz_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите произвольное слово: ");
+            string? a = Console.ReadLine(); ;
+            Console.Write("Введите произвольное слово: ");
+            string? b = Console.ReadLine();
+
+            string aa = String.Concat(a.OrderBy(c => c));
+            string bb = String.Concat(b.OrderBy(c => c));
+
+            if (aa == bb)
+            {
+                Console.WriteLine("Слова являются анаграммой");
+            }
+            else
+            {
+                Console.WriteLine("Слова не являются анаграммой");
+            }
+        }
+    }
+}
