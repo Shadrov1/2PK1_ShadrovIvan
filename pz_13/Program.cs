4a1beaa15b7789cabaee5d1604c309fe9bb12067
Shadrov Ivan <114126714+Shadrov1@users.noreply.github.com>
2022-11-14 12:00:10 +0500

    Add files via upload

---
+++ pz_13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_13
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ваш рост (cм.)?");
+            double rost = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ваш вес (кг.)?");
+            double ves = Double.Parse(Console.ReadLine());
+
+            double imt = (ves / (rost * rost))*10000;
+
+            Console.WriteLine("Ваш индекс массы тела: " + imt);
+            Console.ReadLine();
+            //var 24
+        }
+    }
+}
