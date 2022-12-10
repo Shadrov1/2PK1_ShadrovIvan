a9727c753689f383fe118c988bd09df5db546209
Иван Шадров <dazooop33@gmail.com>
2022-12-10 09:11:44 +0500

    var 6

---
+++ pz_17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public static class Extensions
+{
+            public static string Reverse(this string str)
+            {
+                char[] chars = str.ToCharArray();
+                Array.Reverse(chars);
+                return new string(chars);
+            }
+}
+
+        public class Example
+        {
+            public static void Main()
+            {
+                string str = "тевирп";
+
+                string reverse = str.Reverse();
+                Console.WriteLine(reverse);
+                Console.ReadLine();
+            }
+        }
