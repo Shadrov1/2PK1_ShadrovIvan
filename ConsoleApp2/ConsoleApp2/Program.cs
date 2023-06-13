bc0890acee46fc36bd08fef4b30e4b4b736cf041
Shadrov Ivan <114126714+Shadrov1@users.noreply.github.com>
2023-06-13 18:11:16 +0500

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Proekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Введите первое число:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int result = num1 + num2;
+            Console.WriteLine($"Результат:" + result);
+            Console.ReadKey();
+        }
+    }
+}
